9388e12e1958fa8a4f3da8aaee52fb465443e959
dunow <dunow33@hotmail.com>
2021-02-07 01:44:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+document.querySelector('main#main').remove();
+
+let header1 = document.createElement('h1');
+header1.setAttribute("id", "victory")
+header1.innerHTML = "Brian is the champion";
+document.body.appendChild(header1);
+
+const newHeader = document.querySelector('h1#victory');
